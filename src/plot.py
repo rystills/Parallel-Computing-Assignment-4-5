e91e773c8478f3d0507ae76f46b0798a4540ceb6
Ryan Stillings <rystills@gmail.com>
2019-04-08 15:08:24 -0400

    batch generate heatmaps, add color bar

--- src/plot.py
+++ src/plot.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
+import os
 
-infile = open("heatmap.out")
-data = [ [int(i) for i in line.strip().split(" ")] for line in infile]
-infile.close()
+hmNames = ["hm0.out","hm25.out","hm50.out","hm75.out"]
+heatmapDir = cwd = os.getcwd().replace('\\','/')[:-4] + "/heatmaps attempt 3/final state only"
 
-plt.pcolormesh(data);
-plt.savefig("heatmap.png");
+for fn in hmNames:
+    print("loading {0}".format(fn))
+    infile = open("{0}/{1}".format(heatmapDir,fn))
+    data = [ [int(i) for i in line.strip().split(" ")] for line in infile]
+    infile.close()
+    heatmap = plt.pcolormesh(data)
+    cb = plt.colorbar(heatmap)
+    plt.savefig(fn[:-4]+".png")
+    cb.remove()
+    print("finished {0}".format(fn))
